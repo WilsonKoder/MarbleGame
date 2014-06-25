c17af709f6cb9be8d525a6811795e2900f4d23cb
WilsonKoder111 <WilsonKoder@Wilsons-MacBook-Pro.local>
2014-06-25 22:17:04 +0800

    Change up structure.

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
-	public float score;
+	public Transform Camera;
+	public Transform Light;
+	public int score;
+	public bool hasWon = false;
+	public bool isPaused = false;
+	public bool hasFallen = false;
 
 	// Use this for initialization
 	void Start () {
 		score = 0;
+		hasWon = false;
+		isPaused = false;
+		hasFallen = false;
 	}
 
 	// Update is called once per frame
@@ -30,63 +38,62 @@ public class PlayerScript : MonoBehaviour {
 			}
 		}
 
-		/*
-		if(Input.GetKey (KeyCode.P)) {
-			if(Time.timeScale != 0f) {
-				Time.timeScale = 0f;
-			}
-			if(Time.timeScale = 0f) {
-				Time.timeScale = 1f;
-			}
-		}
+		Light.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z - 1);
+		Camera.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z - 2);
 
-		//rigidbody.AddForce(Vector3.back * Input.GetKey (KeyCode.S) * 10);
-		//rigidbody.AddForce(Vector3.left * Input.GetKey (KeyCode.A) * 10);
-		//rigidbody.AddForce(Vector3.right * Input.GetKey (KeyCode.D) * 10);
-		if(Input.GetKey(KeyCode.W)) {
-			//transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.1f);
-			rigidbody.AddForce(Vector3.forward * Input.GetKey (KeyCode.W) * 10);
-		}
-		if(Input.GetKey(KeyCode.D)) {
-			transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
-		}
-		if(Input.GetKey(KeyCode.A)) {
-			transform.position = new Vector3(transform.position.x - 0.1f, transform.position.y, transform.position.z);
-		}
-		if(Input.GetKey(KeyCode.S)) {
-			transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.1f);
-		}
 		if(transform.position.y < -5) {
-			score = score - 1;
-			transform.position = new Vector3(0.0f, 1.5f, transform.position.z);
+			hasFallen = true;
 		}
-		if(Input.GetKeyDown (KeyCode.Space)) {
-			transform.position = new Vector3(transform.position.x, transform.position.y + 0.2f, transform.position.z);
-		} else if(Input.GetKeyUp (KeyCode.Space)) {
-			transform.position = new Vector3(transform.position.x, transform.position.y - 0.2f, transform.position.z);
-		}
-
-		if(Input.GetKey (KeyCode.P)) {
-			if(Time.timeScale != 0.0f) {
-				Time.timeScale = 0.0f;
-			}
-			if(Time.timeScale = 0.0f) {
-				Time.timeScale = 1.0f;
-			}
-		}
-		*/
 	}
-	/*
 	void OnGUI() {
 		GUI.Label (new Rect(10, 10, 100, 100), "Score = " + score);
 
-		if(Time.timeScale = 0.0f) {
-			if(GUI.Button(new Rect(100, 100, 100, 100), "Unpause Game")) {
-				Time.timeScale = 1.0f;
-				Debug.Log ("User has paused the game");
+		if(isPaused) {
+			if(GUI.Button(new Rect(100, 100, 300, 100), "Unpause Game")) {
+				Pause ();
+				Debug.Log ("User has unpaused the game");
+			}
+			if(GUI.Button (new Rect(100, 210, 300, 100), "Quit game?")) {
+				Application.Quit();
 			}
 		}
-
+		if(hasWon && !isPaused) {
+			if(GUI.Button (new Rect(100, 100, 300, 100), "Advance to next level")) {
+				//Application.LoadLevel();
+			}
+			if(GUI.Button (new Rect(100, 210, 300, 100), "Quit game?")) {
+				Application.Quit();
+			}
+		}
+		if(hasFallen) {
+			if(GUI.Button(new Rect(100, 100, 300, 100), "Retry?")) {
+				Debug.Log ("User has clicked retry");
+				transform.position = new Vector3(1, 2, -2);
+				hasFallen = false;
+				score = score - 1;
+			}
+			if(GUI.Button (new Rect(100, 210, 300, 100), "Quit game?")) {
+				Application.Quit();
+			}
+		}
 	}
-	*/
+
+	void OnTriggerEnter() {
+		Debug.Log ("You win!");
+		hasWon = true;
+		score++;
+	}
+	void Pause() {
+		if(Input.GetKey (KeyCode.Escape)) {
+			if(isPaused) {
+				Time.timeScale = 1;
+				Debug.Log ("Application unpaused");
+				isPaused = false;
+			} else if (!isPaused) {
+				Time.timeScale = 0;
+				Debug.Log ("Application paused");
+				isPaused = true;
+			}
+		}
+	}
 }
